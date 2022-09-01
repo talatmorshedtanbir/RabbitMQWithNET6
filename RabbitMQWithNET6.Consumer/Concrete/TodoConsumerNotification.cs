7e29f5305e31112fbb3169a875b3e62e8f36fd94
Talat Tanbir <hossaintanbir0@gmail.com>
2022-09-02 01:44:26 +0600

    Add Notification Service

---
+++ RabbitMQWithNET6.Consumer/Concrete/TodoConsumerNotification.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using RabbitMQWithNET6.Common.Commands;
+
+namespace RabbitMQWithNET6.Consumer.Concrete
+{
+    public class TodoConsumerNotification : IConsumer<TodoCommandMessage>
+    {
+        public async Task Consume(ConsumeContext<TodoCommandMessage> context)
+        {
+            var message = context.Message;
+            await Console.Out.WriteLineAsync($"Message from Producer : {message.todoModel.TaskDescription}");
+        }
+    }
+}
---
+++ RabbitMQWithNET6.Notification/Program.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using RabbitMQWithNET6.Common.Settings;
+using RabbitMQWithNET6.Consumer.Concrete;
+
+static void Main(string[] args)
+{
+    var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+    IConfiguration config = builder.Build();
+
+    // Configure Rabbit MQ
+    var rabbitMqSettings = config.GetSection(nameof(RabbitMqSettings)).Get<RabbitMqSettings>();
+
+    Console.Title = "Notification";
+
+    var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+    {
+        cfg.Host(new Uri(rabbitMqSettings.Uri), h =>
+        {
+            h.Username(rabbitMqSettings.UserName);
+            h.Password(rabbitMqSettings.Password);
+        });
+        cfg.ReceiveEndpoint(rabbitMqSettings.TodoQueue, ep =>
+        {
+            ep.PrefetchCount = 16;
+            ep.UseMessageRetry(r => r.Interval(2, 100));
+            ep.Consumer<TodoConsumerNotification>();
+        });
+
+    });
+
+    bus.StartAsync();
+    Console.WriteLine("Listening for Todo registered events.. Press enter to exit");
+    Console.ReadLine();
+    bus.StopAsync();
+}
